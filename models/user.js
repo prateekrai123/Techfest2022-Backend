98348a428fda7143e597ab12884dd529d24ae13f
Prateek rai <1810256@sliet.ac.in>
2021-12-17 10:19:49 +0530

    added user model

---
+++ models/user.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.userSchema({
+    firstName: {
+        type: String,
+        trim: true,
+        requires: true,
+        maxLength: 32
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        requires: true,
+        maxLength: 32
+    },
+    dob: {
+        type: Date,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    phone: {
+        type: String,
+        maxlength: 13,
+        trim: true,
+    },
+    whatsappPhoneNumber: {
+        type: String,
+        maxlength: 15,
+        trim: true,
+    },
+    telegramPhoneNumber: {
+        type: String,
+        maxlength: 15,
+        trim: true,
+    },
+    collegeName: {
+        type: String,
+        maxlength: 200,
+        trim: true
+    },
+    regNo: {
+        type: String,
+        maxlength: 10,
+        trim: true
+    },
+    instituteAddress: {
+        type: String,
+        maxlength: 200,
+        trim: true
+    },
+    course: {
+        //Name of course like B.E., B.Tech etc..
+        type: String,
+        maxlength: 100,
+        trim: true
+    },
+    yearOfStudy: {
+        type: Number
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    userId: {
+        type: String,
+    }
+})
