0ab7fbd914fbf12d263ca645fcaa06b7e1157797
shoaibisa <shoaibisa1@gmail.com>
2022-03-18 20:04:06 +0530

    delete coordinator

--- controllers/coordinator.js
+++ controllers/coordinator.js
@@ -1,8 +1,7 @@
 const Coordinator = require("../models/coordinator");
 const { validationResult } = require("express-validator");
 const path = require("path");
-const multer = require("multer");
-const { json } = require("body-parser");
+const fs = require("fs");
 const { failAction, successAction } = require("../utils/response");
 const fileHelper = require("../utils/file");
 
@@ -130,27 +129,29 @@ exports.updateGetCoordinator = (req, res, next) => {
 };
 
 exports.deleteCoordinator = (req, res, next) => {
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array() });
-  }
   const cid = req.params.cid;
+
+  // return console.log(cid);
 
   Coordinator.findByIdAndDelete(cid)
     .then((result) => {
       if (!result) {
-        res.status(404).json(failAction("Not found! "));
+        res.status(208).json({
+          isError: true,
+          title: "Error",
+          message: "Image is not given",
+        });
       }
       const pathImg = "upload/images/" + result.photo;
-      fileHelper.deleteFiles(pathImg);
-      res.status(410).json(
-        successAction({
-          data: result,
-          statusCode: 410,
-          message: "successfully deleted! ",
-        })
-      );
+      if (fs.existsSync(pathImg)) {
+        fileHelper.deleteFiles(pathImg);
+      } //photo exists
+
+      res.status(208).json({
+        data: result,
+        statusCode: 410,
+        message: "successfully deleted! ",
+      });
     })
     .catch((err) => {
       failAction("Not found! ");
