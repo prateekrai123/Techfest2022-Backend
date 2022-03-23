92180468f9de8d59f309d4e016ee58bac21706cd
shoaibisa <shoaibisa1@gmail.com>
2022-03-23 14:29:25 +0530

    ff

--- controllers/pay.js
+++ controllers/pay.js
@@ -75,3 +75,32 @@ exports.successPay = async (req, res) => {
 exports.failPay = (req, res) => {
   res.render("failedpay");
 };
+
+exports.failPpay = (req, res) => {
+  res.render("failedpay");
+};
+exports.sucPay = (req, res) => {
+  res.render("successpay");
+};
+
+// exports.hasPaymendSuccess = (req, res, next) => {
+//   const uId = req.userId;
+//   const getPaymentUser = await User.findById({ _id: uId });
+//   const checkIfPaid = await Stripe.checkout.sessions.retrieve(
+//     getPaymentUser.paymentDetails.paymentId
+//   );
+//   if (!checkIfPaid) {
+//     return res
+//       .status(208)
+//       .json({ isError: true, message: "Some Error Accured" });
+//   }
+
+//   if (checkIfPaid.payment_status != "unpaid") {
+//     next();
+//   }
+//   return res.status(208).json({
+//     isError: true,
+//     title: "Not Paid",
+//     message: "You have to pay first!",
+//   });
+// };
--- routes/pay.js
+++ routes/pay.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post("/user", isAuth, payCon.payUser);
 router.get("/success", payCon.successPay);
 router.get("/fail", payCon.failPay);
-
+router.get("/suc", payCon.sucPay);
+router.get("/fl", payCon.failPpay);
 module.exports = router;
