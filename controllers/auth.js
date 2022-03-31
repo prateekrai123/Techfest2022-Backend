2ef7fe22a12c2d0c151e8f5778009b698d372272
Prateek rai <1810256@sliet.ac.in>
2022-03-31 10:12:07 +0530

    finding bug

--- controllers/auth.js
+++ controllers/auth.js
@@ -391,7 +391,7 @@ exports.changeForgotPassword = (req, res) => {
     try {
       ejs.renderFile("../views/forgotPassword.ejs", { email: token.email });
     } catch (err) {
-      return res.status(404).json("Some error occured");
+      return res.status(404).json("Some error occured" + err);
     }
   });
 };
