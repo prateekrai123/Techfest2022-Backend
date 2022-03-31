7aff87ff4c123a358ea27697f7a302a96aca972b
Prateek rai <1810256@sliet.ac.in>
2022-03-31 10:27:53 +0530

    fixing rendering

--- controllers/auth.js
+++ controllers/auth.js
@@ -390,7 +390,7 @@ exports.changeForgotPassword = (req, res) => {
     }
 
     try {
-      res.renderFile("views/forgotPassword.ejs", { email: token.email });
+      res.render("views/forgotPassword.ejs", { email: token.email });
     } catch (err) {
       return res.status(404).json("Some error occured" + err);
     }
